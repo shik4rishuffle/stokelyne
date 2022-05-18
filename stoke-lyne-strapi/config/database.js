60e8885dc5c73511b409b6fb698ff5e8879bb8d5
Flynn Davies <fdavies.freelance@gmail.com>
2022-05-18 12:42:27 +0100

    add production env to strapi

--- stoke-lyne-strapi/config/database.js
+++ stoke-lyne-strapi/config/database.js
@@ -13,3 +13,34 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
+    };
+  }
+
+  return {
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    }
+  }  
+};
