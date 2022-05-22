0c31030a09e5bf1cd88ae1749f97c57601ff7a1c
Flynn Davies <fdavies.freelance@gmail.com>
2022-05-22 15:31:37 +0100

    split prod to seperate file

--- stoke-lyne-strapi/config/database.js
+++ stoke-lyne-strapi/config/database.js
@@ -1,31 +1,15 @@
-module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME', 'strapi'),
-          password: env('DATABASE_PASSWORD', 'strapi'),
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-    };
-  }
-
-  return {
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      options: {
+        useNullAsDefault: true,
       },
-      useNullAsDefault: true,
-    }
-  }  
-};
- 
+    },
+  },
+});
