58971e2642b246236b8c322295e83aff060b2c07
Flynn Davies <fdavies.freelance@gmail.com>
2022-05-22 15:17:31 +0100

    Update database.js

--- stoke-lyne-strapi/config/database.js
+++ stoke-lyne-strapi/config/database.js
@@ -1,19 +1,3 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
-
 module.exports = ({ env }) => {
   if (env('NODE_ENV') === 'production') {
     return {
@@ -44,3 +28,4 @@ module.exports = ({ env }) => {
     }
   }  
 };
+ 
