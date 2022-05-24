70bb3343f05b4aa23f59a1737d48be4180c63991
Flynn Davies <fdavies.freelance@gmail.com>
2022-05-24 13:30:20 +0100

    Update database.js

--- stoke-lyne-strapi/config/env/production/database.js
+++ stoke-lyne-strapi/config/env/production/database.js
@@ -1,17 +1,30 @@
-module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          ca: env('DATABASE_CA')
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
         },
+        debug: false,
       },
-      debug: true,
-    },
-  });
+    };
+  }
+
+  return {
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    }
+  }  
+};
