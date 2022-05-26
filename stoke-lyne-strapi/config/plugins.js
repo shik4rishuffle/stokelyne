c62197c757707011ab7803798a963608763f8514
Flynn Davies <fdavies.freelance@gmail.com>
2022-05-26 17:18:48 +0100

    add .env; formatting

--- stoke-lyne-strapi/config/plugins.js
+++ stoke-lyne-strapi/config/plugins.js
@@ -1,13 +1,13 @@
-module.exports = ({env}) => ({
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-do", 
-        providerOptions: {
-          key: env('DO_SPACE_ACCESS_KEY'),
-          secret: env('DO_SPACE_SECRET_KEY'),
-          endpoint: env('DO_SPACE_ENDPOINT'),
-          space: env('DO_SPACE_BUCKET')
-        }
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: process.env.DO_SPACE_ACCESS_KEY,
+        secret: process.env.DO_SPACE_SECRET_KEY,
+        endpoint: process.env.DO_SPACE_ENDPOINT,
+        space: process.env.DO_SPACE_BUCKET
       },
-    }, 
-  })
+    },
+  },
+})
