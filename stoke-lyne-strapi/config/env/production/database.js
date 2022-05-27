30395cda862e0b18da3deecddbe507e34460090c
Flynn Thomas Davies <flynn.davies@immediate.co.uk>
2022-05-27 14:59:21 +0100

    Update database.js

--- stoke-lyne-strapi/config/env/production/database.js
+++ stoke-lyne-strapi/config/env/production/database.js
@@ -1,30 +1,19 @@
-module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    return {
-      connection: {
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
         client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME', 'strapi'),
-          password: env('DATABASE_PASSWORD', 'strapi'),
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: true,
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
-    };
-  }
-
-  return {
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      options: {
+        ssl: false,
       },
-      useNullAsDefault: true,
-    }
-  }  
-};
+    },
+  },
+});
