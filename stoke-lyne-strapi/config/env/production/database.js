1d99071892ad17f79cb533c11e4f6b2967189b57
Flynn Thomas Davies <flynn.davies@immediate.co.uk>
2022-05-27 17:06:13 +0100

    Update database.js

--- stoke-lyne-strapi/config/env/production/database.js
+++ stoke-lyne-strapi/config/env/production/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'stokelyne'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'password'),
       },
       options: {
         ssl: false,
