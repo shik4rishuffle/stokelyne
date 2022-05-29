61d4b622474ea41b6476fd7ee0a1d3b5e1c81779
Flynn Thomas Davies <flynn.davies@immediate.co.uk>
2022-05-29 17:57:57 +0100

    Update UserTable.jsx

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Container, Row, Col, Image} from "react-bootstrap";
-import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image';
+import {Container, Row, Col} from "react-bootstrap";
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export default function UserTable(props) {
   console.log('hit');
@@ -11,7 +11,7 @@ export default function UserTable(props) {
       props.data.edges.map(user => (
       <Row paddingTop="10px">
         <Col xs={12} md={2}>
-          <GatsbyImage image={getImage(user.node.DisplayPicture.localFile)} objectFit="cover" loading="lazy" alt=""></GatsbyImage>
+          <GatsbyImage image={getImage(user.node?.DisplayPicture?.localFile)} objectFit="cover" loading="lazy" alt=""></GatsbyImage>
         </Col>
         <Col paddingTop="10px">
           <Row className="h4 capitalize">{user.node.name}</Row>
