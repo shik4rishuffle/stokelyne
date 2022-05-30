20c7a32587d5d2e005dfb3cae6ff91a72f96d6d0
Flynn Thomas Davies <flynn.davies@immediate.co.uk>
2022-05-30 20:43:23 +0100

    add uploader dos

--- stoke-lyne-strapi/config/env/production/plugins.js
+++ stoke-lyne-strapi/config/env/production/plugins.js
@@ -1,12 +1,13 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "strapi-provider-upload-do",
+      provider: "strapi-provider-upload-dos",
       providerOptions: {
         key: process.env.DO_SPACE_ACCESS_KEY,
         secret: process.env.DO_SPACE_SECRET_KEY,
         endpoint: process.env.DO_SPACE_ENDPOINT,
-        space: process.env.DO_SPACE_BUCKET
+        space: process.env.DO_SPACE_BUCKET,
+        directory: "/media",
       },
     },
   },
