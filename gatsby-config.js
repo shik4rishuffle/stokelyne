f79dddb67746e81a274ea4cdf991eb8087ce3309
Flynn Thomas Davies <flynn.davies@immediate.co.uk>
2022-06-01 11:24:04 +0100

    fix branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,69 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: "https://www.stokelyne.com",
-        title: "stoke lyne"
-    },
-    flags: {
-        DEV_SSR: false
-    },
-    plugins: [
-        `gatsby-transformer-remark`,
-        `gatsby-plugin-image`,
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp',
-        `gatsby-plugin-anchor-links`,
-        {
-            resolve: "gatsby-plugin-anchor-links",
-            options: {
-                offset: -100,
-                duration: 300
-            },
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `projects`,
-                path: `${__dirname}/src/content/`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images/`,
-            }
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `minutes`,
-                path: `${__dirname}/src/assets/parish/minutes`,
-            }
-        },
-        {
-            resolve: "gatsby-source-strapi",
-            options: {
-                apiURL: process.env.APIURL,
-                collectionTypes: [
-                    "blog-posts",
-                    "blog-categories",
-                    "events",
-                    "downloads",
-                    "basic-pages",
-                    "minutes",
-                    "parish-members"
-                ],
-                singleTypes: [
-                    "home-page"
-                ],
-                queryLimit: 1000,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-                fonts: [
-                    `poppins:300,400,500,600,700`
-                ], display: 'swap'
-            }
-        }
-    ]
+	siteMetadata: {
+		siteUrl: "https://www.stokelyne.com",
+		title: "stoke lyne"
+	},
+	flags: {
+		DEV_SSR: false
+	},
+	plugins: [
+		"gatsby-transformer-remark",
+		"gatsby-plugin-image",
+		"gatsby-transformer-sharp",
+		"gatsby-plugin-sharp",
+		"gatsby-plugin-anchor-links",
+		{
+			resolve: "gatsby-plugin-anchor-links",
+			options: {
+				offset: -100,
+				duration: 300
+			},
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "projects",
+				path: `${__dirname}/src/content/`,
+			},
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: `${__dirname}/src/images/`,
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "minutes",
+				path: `${__dirname}/src/assets/parish/minutes`,
+			}
+		},
+		{
+			resolve: "gatsby-source-strapi",
+			options: {
+				apiURL: process.env.APIURL,
+				collectionTypes: [
+					"blog-posts",
+					"blog-categories",
+					"events",
+					"downloads",
+					"basic-pages",
+					"minutes",
+					"parish-members"
+				],
+				singleTypes: [
+					"home-page"
+				],
+				queryLimit: 1000,
+			},
+		},
+		{
+			resolve: "gatsby-plugin-google-fonts",
+			options: {
+				fonts: [
+					"poppins:300,400,500,600,700"
+				], display: "swap"
+			}
+		}
+	]
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,37 @@
 import React, {Component} from "react";
 
 class Footer extends Component {
-  render() {
-    return (
-    <section id="footer" className="footer-component">
-      <div className="contact">
-        <div className="contact-left">
-        <h3>Get in touch</h3>
-        <div className="contact-form">
-          <form>
-            <div className="form-group">
-              <input className="contact-form__name" placeholder="Name"></input>
-              <input className="contact-form__email" placeholder="Email"></input>
-            </div>
-            <input className="contact-form__subject" placeholder="Subject"></input>
-            <textarea className="contact-form__body" placeholder="Message"></textarea>
-          </form>
-        </div>
-        </div>
-        <div className="contact-right">
-          <h3>
-            Come find us
-          </h3>
-          <div className="map-responsive">
-            <iframe height="100%" style="border:0" allowFullScreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39342.29088210565!2d-1.2191874434414014!3d51.95413935590053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876de1382d97ccb%3A0xc7fa8ac11ee480c7!2sStoke%20Lyne%2C%20Bicester!5e0!3m2!1sen!2suk!4v1651854066132!5m2!1sen!2suk" loading="lazy"></iframe>
-          </div>
-        </div>
-      </div>
-    </section>
-    );
-  }
+	render() {
+		return (
+			<section id="footer" className="footer-component">
+				<div className="contact">
+					<div className="contact-left">
+						<h3>Get in touch</h3>
+						<div className="contact-form">
+							<form>
+								<div className="form-group">
+									<input className="contact-form__name" placeholder="Name"></input>
+									<input className="contact-form__email" placeholder="Email"></input>
+								</div>
+								<input className="contact-form__subject" placeholder="Subject"></input>
+								<textarea className="contact-form__body" placeholder="Message"></textarea>
+							</form>
+						</div>
+					</div>
+					<div className="contact-right">
+						<h3>
+                            Come find us
+						</h3>
+						<div>
+							<iframe
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39342.29088210565!2d-1.2191874434414014!3d51.95413935590053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876de1382d97ccb%3A0xc7fa8ac11ee480c7!2sStoke%20Lyne%2C%20Bicester!5e0!3m2!1sen!2suk!4v1651854066132!5m2!1sen!2suk"
+								loading="lazy"></iframe>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Footer;
