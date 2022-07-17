37e413da6f7cf67903c41814435ed1310c6cc670
Flynn Thomas Davies <flynn.davies@immediate.co.uk>
2022-07-17 14:45:29 +0100

    update all pages lifecycles

--- stoke-lyne-strapi/api/downloads/models/downloads.js
+++ stoke-lyne-strapi/api/downloads/models/downloads.js
@@ -1,8 +1,49 @@
 'use strict';
+const { exec } = require("child_process");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    afterCreate: async(event) => {
+      console.log('hit');
+      const { result, params } = event;
+      exec(`cd ../ && yarn build && pm2 restart all`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log('rebuilt from creation');
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+    },
+    afterUpdate: async(event) => {
+      const { result, params } = event;
+      exec(`cd ../ && yarn build && pm2 restart all`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log('rebuilt from changes');
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+    },
+    afterDelete: async(event) => {
+      const { result, params } = event;
+      exec(`cd ../ && yarn build && pm2 restart all`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log('rebuilt from delete');
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+    }
+  }
+};
